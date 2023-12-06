24011196a00809f1d6562a416fb23d0391ea877a
ivanebernal <github@ivanesparza.dev>
2023-12-05 23:15:07 -0800

    Day 3

---
+++ 03/part1/gear-ratios.kts
@@ -0,0 +1,53 @@
+import java.io.File
+
+val file = if(args.contains("-i")) args[1 + args.indexOf("-i")] else ""
+
+val schematic = File(file).readLines().map { line -> line.toCharArray().toList() }
+
+val rows = schematic.size
+val cols = schematic[0].size
+
+var result = 0
+
+for(row in 0 until rows) {
+    var currentNum = ""
+    for(col in 0 until cols) {
+        if(schematic[row][col].isDigit()) {
+            currentNum += schematic[row][col]
+        } else {
+            if (
+                !currentNum.isEmpty() &&
+                isAdjacentToGear(row, col - currentNum.length, col - 1)
+            ) {
+                result += currentNum.toInt()
+            }
+            currentNum = ""
+        }
+    }
+    if (
+        !currentNum.isEmpty() &&
+        isAdjacentToGear(row, cols - currentNum.length, cols - 1)
+    ) {
+        result += currentNum.toInt()
+    }
+}
+
+fun isAdjacentToGear(row: Int, start: Int, end: Int): Boolean {
+    val range = start - 1 .. end + 1
+    for (r in row - 1 .. row + 1) {
+        for (c in range) {
+            if(isInRange(r, c) && isGear(r, c)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun isInRange(row: Int, col: Int): Boolean = 
+    row >= 0 && row < rows && col >= 0 && col < cols
+
+fun isGear(row: Int, col: Int): Boolean =
+    !schematic[row][col].isDigit() && schematic[row][col] != '.'
+
+println("Sum of parts: $result")
